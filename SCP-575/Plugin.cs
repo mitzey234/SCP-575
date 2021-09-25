26fac1b4ae2e396b29092e14ced731cd27d3c587
Marco15453 <marcolpder@gmail.com>
2021-09-25 23:45:37 +0200

    Update Plugin.cs

--- SCP-575/Plugin.cs
+++ SCP-575/Plugin.cs
@@ -17,7 +17,7 @@ namespace SCP_575
 		public override string Author { get; } = "Galaxy119";
 		public override string Name { get; } = "SCP-575";
 		public override string Prefix { get; } = "575";
-		public override Version Version { get; } = new Version(3, 7, 0);
+		public override Version Version { get; } = new Version(3, 8, 0);
 		public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
 		
 		public Random Gen = new Random();
